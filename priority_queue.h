abd07f3820ed1be97db8786b52d2817700a8bec5
Abhishek-thakur1 <abhishekcool1804@gmail.com>
2022-09-09 12:08:31 +0530

    initial-commit:

---
+++ priority_queue.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include<iostream>
+#include<limits>
+#include<memory>
+
+#ifndef PROJECT_PRIORITY_QUEUE_H
+#define PROJECT_PRIORITY_QUEUE_H
+
+namespace jw{
+
+    typedef struct PriorityQueueElement{
+        int key_;
+        std::string value_;
+    }PQElement;
+
+    
+};
+
+#endif
